2f6831265eec3ae91df18a029f8518da7553abf0
Thando <nodwengut@gmail.com>
2019-10-31 13:02:20 +0200

    added db structure

---
+++ table.sql
@@ -0,0 +1,19 @@
+
+
+CREATE TABLE users(
+    id SERIAL NOT NULL  PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL, 
+    usertype TEXT NOT NULL,
+    phone TEXT NOT NULL,
+);
+
+CREATE TABLE cars(
+    id SERIAL NOT NULL  PRIMARY KEY,
+    seats TEXT NOT NULL,
+    reg_number TEXT NOT NULL, 
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
